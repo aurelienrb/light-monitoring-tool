17b7105e376445f055717c4d1a72211a0789410d
Aurelien <github@cyberkarma.net>
2016-10-23 00:51:34 +0200

    Basic http server

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <server_http.hpp>
 
-int main() {
+typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
 
+int main() {
+	HttpServer server(8080, 1);
+	server.config.address = "127.0.0.1";
+
+	server.resource["^/$"]["GET"] = [](std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) {
+		const std::string result = "OK!";
+		*response << "HTTP/1.1 200 OK\r\nContent-Length: " << result.length() << "\r\n\r\n" << result;
+	};
+
+	server.start();
 }
