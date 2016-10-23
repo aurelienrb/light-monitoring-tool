bdf46ed34e4929fb3e96228840e4c40440cbdb92
Aurelien <github@cyberkarma.net>
2016-10-23 06:01:11 +0200

    Better stats display (last 300 seconds) Handle loss of connection

--- src/cpu-stats.cpp
+++ src/cpu-stats.cpp
@@ -2,30 +2,38 @@
 
 #include <sstream>
 #include <cstdlib>
+#include <algorithm>
+#include <iomanip>
 
 std::vector<int> getCPUStats() {
 	static std::vector<int> stats;
-	const size_t nbValues = 60;
+	const size_t nbValues = 300;
 	if (!stats.empty()) {
 		stats.assign(stats.begin() + 1, stats.end());
+		stats.resize(stats.size() - 1);
 	}
 	while (stats.size() < nbValues) {
-		const int n = std::rand() % 10;
+		const int n = (std::rand() % 100);
 		stats.push_back(n);
 	}
 	return stats;
 }
 
-//const std::string result = "{ \"cpu\": [10, 15, 27, 17, 54, 68, 77, 61, 89, 47, 54, 63, 57, 65, 74, 88, 97, 95, 91, 98, 92, 84, 75, 31, 12, 5, 9, 5, 11, 4, 6, 8, 4] }";
-
 std::string to_json(const std::vector<int> & data) {
 	std::ostringstream oss;
-	oss << "{ \"cpu\": [";
+	oss << "{ "
+		<< std::quoted("label") << ": " << std::quoted("CPU") << ", "
+		<< std::quoted("data") << ": [";
+
 	bool isFirst = true;
-	for (int n : data) {
-		oss << (isFirst ? "" : ", ") << n;
+	for (size_t i = 0; i < data.size(); ++i) {
+		if (i != 0) {
+			oss << ", ";
+		}
+		oss << "[" << i+1 << ", " << data[i] << "]";
 		isFirst = false;
 	}
+
 	oss << "] }";
 	return oss.str();
 }
